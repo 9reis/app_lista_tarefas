e302bad7a9620463e163c0aee2ac90d0dbfd5397
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-09 21:07:43 -0300

    Personalizando o campo de Texto

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,31 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          //padding: const EdgeInsets.only(left: 16, right:16),
+          //padding: const EdgeInsets.symmetric(horizontal:10 // vertical:10),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: "E-mail",
+              hintText: "teste@teste.com", // Igual ao Placeholder
+              border: OutlineInputBorder(), //InputBorder.none
+              errorText: "Campo obrigatório",
+              prefixText: 'R\$ ',
+              labelStyle: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            obscureText: true, //Oculta os caracters
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.purple,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
