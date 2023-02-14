b61e31eac1b6979b9b8a026603c8f71ce6266ded
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-14 17:26:28 -0300

    Package flutter_slidable

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -2,6 +2,7 @@ import 'package:app_lista_tarefas/models/todo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 
 class TodoListItem extends StatelessWidget {
@@ -11,24 +12,38 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[200],
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const BehindMotion(),
+        children: [
+          SlidableAction(
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            label: 'Delete',
+            onPressed: ((context) {}),
+          ),
+        ],
       ),
-      margin: const EdgeInsets.symmetric(vertical: 3),
-      padding: EdgeInsets.all(16),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(DateFormat('dd/MM/yyy - HH:mm').format(todo.dateTime),
-            style: TextStyle(
-              fontSize: 12,
-            )),
-        Text(todo.title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            )),
-      ]),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 3),
+        padding: EdgeInsets.all(16),
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Text(DateFormat('dd/MM/yyy - HH:mm').format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              )),
+          Text(todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              )),
+        ]),
+      ),
     );
   }
 }
